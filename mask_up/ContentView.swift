04e6dde08c36f00c05ec4fb1dc076fb72d63ffa6
farzamnoori <farzamn@icloud.com>
2020-08-11 20:02:14 -0600

    first commit

---
+++ mask_up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mask_up
+//
+//  Created by Farzam Noori on 2020-08-11.
+//  Copyright © 2020 Farzam Noori. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
